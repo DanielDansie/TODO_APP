ec36c82084f497b63b2b1732e271420f16f0894a
Daniel Dansie <dan.dansie126@gmail.com>
2022-09-21 10:16:11 -0600

    added new branch to test different JS

--- script.js
+++ script.js
@@ -1,54 +1,27 @@
-document.querySelector('#createBtn').onclick = function(){
-    if(document.querySelector('#toDoListStart input').value.length == 0){
-        alert("You Must Enter Task Name To Add.")
-    }
+let toDoItemNames = [];
 
-    else{
-        document.querySelector('.list-group').innerHTML += `
-            <li class="list-group-item" id="${document.querySelector('#toDoListNameInput').value}>
-                <h3 id="taskname">
-                    ${document.querySelector('#toDoListNameInput').value}
-                </h3>
-                <button class="edit">
-                    <i class="bi bi-pencil-square"></i>
-                </button>
-                <button class="delete">
-                    <i class="bi bi-trash"></i>
-                </button>
-            </li>`;
+function addTodo(text) {
+    const todo = {
+        text,
+        checked: false,
+        id: Date.now(),
+    };
 
-            document.querySelector('#toDoList').innerHTML += `
-            <div class="task">
-                <h3 id="taskname">
-                    ${document.querySelector('#toDoListNameInput').value}
-                </h3>
-            </div>
-            <div class="listItems">
-                <ul class="list-group list-group-flush">
-                    <input id="toDoListItem" placeholder="Input To Do List Steps">
-                    <button id="addItem">Add to do Item</button>
-                </ul>
-            </div>`;
-
-        let editList = document.querySelectorAll(".edit");
-        for(var i=0; i<editList.length; i++) {
-            editList[i].onclick = function() {
-                this.parentNode.editList
-            }
-        }
-
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
-    }
+    toDoItemNames.push(todo);
+    console.log(toDoItemNames)
 }
 
+const createList = document.querySelector('.toDoListInput');
 
-// document.querySelectorAll(".edit").addEventListener('click', (e) => {
-//     if (e.target.className !== 'edit') return
-//     e.target.previousSibling.setAttribute('contenteditable', 'true');
-//     e.target.previousSibling.focus();
-// });
+createList.addEventListener('submit', Event => {
+    // Event.preventDefault();
+
+    const input = document.querySelector('#toDoListNameInput');
+
+    const text = input.value.trim();
+    if (text !== '') {
+        addTodo(text);
+        input.value = '';
+        input.focus();
+    }
+})
