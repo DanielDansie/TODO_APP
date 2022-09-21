b5e69918b747605a4038f01678a746d549c5b5e1
Daniel Dansie <dan.dansie126@gmail.com>
2022-09-21 08:30:01 -0600

    added in the Javascript to add the list item names to toDoList div

--- script.js
+++ script.js
@@ -4,23 +4,36 @@ document.querySelector('#createBtn').onclick = function(){
     }
 
     else{
-        document.querySelector('#toDoList').innerHTML += `
-            <div class="task">
+        document.querySelector('.list-group').innerHTML += `
+            <li class="list-group-item" id="${document.querySelector('#toDoListNameInput').value}>
+                <h3 id="taskname">
+                    ${document.querySelector('#toDoListNameInput').value}
+                </h3>
                 <button class="edit">
                     <i class="bi bi-pencil-square"></i>
                 </button>
-                <span id="taskname">
-                    ${document.querySelector('#toDoListNameInput').value}
-                </span>
                 <button class="delete">
                     <i class="bi bi-trash"></i>
                 </button>
+            </li>`;
+
+            document.querySelector('#toDoList').innerHTML += `
+            <div class="task">
+                <h3 id="taskname">
+                    ${document.querySelector('#toDoListNameInput').value}
+                </h3>
+            </div>
+            <div class="listItems">
+                <ul class="list-group list-group-flush">
+                    <input id="toDoListItem" placeholder="Input To Do List Steps">
+                    <button id="addItem">Add to do Item</button>
+                </ul>
             </div>`;
 
-        let editList = document.querySelector(".edit");
+        let editList = document.querySelectorAll(".edit");
         for(var i=0; i<editList.length; i++) {
             editList[i].onclick = function() {
-                
+                this.parentNode.editList
             }
         }
 
@@ -33,3 +46,9 @@ document.querySelector('#createBtn').onclick = function(){
     }
 }
 
+
+// document.querySelectorAll(".edit").addEventListener('click', (e) => {
+//     if (e.target.className !== 'edit') return
+//     e.target.previousSibling.setAttribute('contenteditable', 'true');
+//     e.target.previousSibling.focus();
+// });
